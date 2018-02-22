260a129c99d82b935e4e0653517093771c975837
arumka <arumka@RIX1503>
2018-02-22 12:40:33 +0200

    Pielabots merge, lai strādātu

---
+++ ESL.CO.React/Models/Column.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESL.CO.React.Models
+{
+    public class Column
+    {
+        public string Name { get; set; }
+        public List<ColumnStatus> Statuses { get; set; }
+    }
+}
--- ESL.CO.React/Models/Value.cs
+++ ESL.CO.React/Models/Value.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,5 +12,24 @@ namespace ESL.CO.React.Models
         //public string Self { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
+
+        public bool Visibility { get; set; }  // vai attēlot attiecīgo Paneli slaidrādē;
+        [Range(1000, 100_000)]
+        public int RefreshRate { get; set; }  // Paneļa pārzīmēšanas laiks sekundēs, pēc kura beigām tiek attēlots tas pats Panelis.
+        [Range(1000, 100_000)]
+        public int TimeShown { get; set; }  // Paneļa attēlošanas laiks sekundēs, pēc kura beigām tiek attēlots nākošais Panelis;
+
+
+        public Value()
+        {
+            Id = 0;
+            Name = "";
+            Type = "";
+            Visibility = false;
+            RefreshRate = 10_000;
+            TimeShown = 1000;
+        }
     }
+
+
 }
