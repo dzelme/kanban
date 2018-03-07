b3cac4034edf5ab9d625b7469e2b3f57d615f2ca
Janis Laimins <janis.laimins@exigenservices.com>
2018-03-07 14:17:17 +0200

    Updated versioning

--- ESL.CO.React/Properties/AssemblyInfo.cs
+++ ESL.CO.React/Properties/AssemblyInfo.cs
@@ -2,4 +2,5 @@
 
 [assembly: AssemblyTitle("ESL.CO.React")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
