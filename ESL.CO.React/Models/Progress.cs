d6bf3a86ecdb032b717bbbfd8d1615b5fd4d40c7
arumka <arumka@RIX1503>
2018-03-14 12:15:02 +0200

    Progress

---
+++ ESL.CO.React/Models/Progress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace ESL.CO.React.Models
+{
+    public class Progress
+    {
+        public int progress { get; set; }
+        public int Total { get; set; }
+        public int Percent { get; set; }
+
+        public Progress()
+        {
+            progress = 0;
+            Total = 0;
+            Percent = 0;
+        }
+    }
+}
