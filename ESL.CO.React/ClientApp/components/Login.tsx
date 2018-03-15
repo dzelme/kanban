e26f0af2969e1da111562f1bcdfad9e8cf424fa5
adzelme <arvids.dzelme@returnonintelligence.com>
2018-03-15 12:21:24 +0200

    KP-27 : pieslēgšanās serviss piešķir JWT tokenu, atgriež vajadzīgās atbildes; KP-26 : vienkāršs login skats KP-27 testēšanai

---
+++ ESL.CO.React/ClientApp/components/Login.tsx
@@ -0,0 +1,56 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import 'isomorphic-fetch';
+import { Credentials } from './Interfaces';
+
+interface LoginState {
+    credentials: Credentials;
+}
+
+export class Login extends React.Component<RouteComponentProps<{}>, LoginState> {
+    constructor() {
+        super();
+        this.state = {
+            credentials: { username: "", password: "" },
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        this.state.credentials.username = document.forms['login'].elements["username"].value;
+        this.state.credentials.password = document.forms['login'].elements["password"].value;
+
+        fetch(' /api/account/login', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state.credentials),
+        })
+            .then(response => {
+                console.log('response:', response.status);
+            });
+    }
+
+    public render() {
+        let contents = Login.renderLogin(this.state.credentials, this.handleSubmit);
+
+        return <div>
+            <h1>Login</h1>
+            {contents}
+        </div>;
+    }
+
+    private static renderLogin(credentials: Credentials, handleSubmit) {
+        return <form name="login" onSubmit={handleSubmit}>
+            <label htmlFor="username">Username</label>
+            <input id="username" name="username" type="text" defaultValue={credentials.username} />
+            <label htmlFor="password">Password</label>
+            <input id="password" name="password" type="password" defaultValue={credentials.password} />
+            <input type="submit" className="btn btn-default" name="Submit" />
+        </form>;
+    }
+}
--- ESL.CO.React/Controllers/AuthenticationController.cs
+++ ESL.CO.React/Controllers/AuthenticationController.cs
@@ -6,37 +6,75 @@ using Microsoft.AspNetCore.Mvc;
 using ESL.CO.React.LdapCredentialCheck;
 using System.Net.Http;
 using System.Net;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using ESL.CO.React.Models;
+using System.Security.Claims;
+using System.Security.Cryptography;
 
 namespace ESL.CO.React.Controllers
 {
-    [Route("api/account")]
+    [Route("api/account/login")]
     public class AuthenticationController : Controller
     {
-        private LdapClient ldap = new LdapClient();
+        //private LdapClient ldap = new LdapClient();
 
-        public AuthenticationController(LdapClient ldap)
+        //public AuthenticationController(LdapClient ldap)
+        //{
+        //    this.ldap = ldap;
+        //}
+
+        //[HttpPost]
+        //public Task<HttpResponseMessage> Login([FromBody]string username, [FromBody]string password)
+        //{
+        //    HttpResponseMessage response = new HttpResponseMessage();
+        //    if (ldap.CheckCredentials(username, password))
+        //    {
+        //        //return 200 + jwt
+        //        response.StatusCode = HttpStatusCode.OK;
+        //    }
+        //    else
+        //    {
+        //        //return 401
+        //        response.StatusCode = HttpStatusCode.Unauthorized;
+        //    }
+
+        //    //response.Content = new StringContent(message);
+        //    return Task.FromResult(response);
+        //}
+
+
+        [HttpPost]
+        public IActionResult IssueJwtToken([FromBody] Credentials credentials)
         {
-            this.ldap = ldap;
+            if (credentials.Username == "adzelme" && credentials.Password == "0TESTtest")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, credentials.Username)
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("bGS6lzFqvvSQ8ALbOxatm7/Vk7mLQyzqaS34Q4oR1ew="));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "rixinterns01.internal.corp/kanban", //http://localhost:58533/
+                    audience: "rixinterns01.internal.corp/kanban",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+
+            return Unauthorized(); //BadRequest instead
         }
 
-        [HttpPost("[action]")]
-        public Task<HttpResponseMessage> Login([FromBody]string username, [FromBody]string password)
-        {
-            HttpResponseMessage response = new HttpResponseMessage(); 
-            if (ldap.CheckCredentials(username, password))
-            {
-                //return 200 + jwt
-                response.StatusCode = HttpStatusCode.OK;
-            }
-            else
-            {
-                //return 401
-                response.StatusCode = HttpStatusCode.Unauthorized;
-            }
 
-            //response.Content = new StringContent(message);
-            return Task.FromResult(response);
-        }
 
         public IActionResult Index()
         {
