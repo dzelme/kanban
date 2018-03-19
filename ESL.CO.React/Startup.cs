4c331768412419226819aab6412c0e8159890652
adzelme <arvids.dzelme@returnonintelligence.com>
2018-03-19 13:25:06 +0200

    dzēsti liekie komentāri

--- ESL.CO.React/Startup.cs
+++ ESL.CO.React/Startup.cs
@@ -46,10 +46,10 @@ namespace ESL.CO.React
                         ValidateAudience = true,
                         ValidateLifetime = true,
                         ValidateIssuerSigningKey = true,
-                        ValidIssuer = Configuration["JwtSettings:Issuer"], //.GetSection("JwtSettings").GetValue<string>("Issuer"),
-                        ValidAudience = Configuration["JwtSettings:Audience"], //.GetSection("JwtSettings").GetValue<string>("Audience"),
+                        ValidIssuer = Configuration["JwtSettings:Issuer"],
+                        ValidAudience = Configuration["JwtSettings:Audience"],
                         IssuerSigningKey = new SymmetricSecurityKey(
-                            Encoding.UTF8.GetBytes(Configuration["JwtSettings:SigningKey"]))  //.GetSection("JwtSettings").GetValue<string>("SigningKey")))
+                            Encoding.UTF8.GetBytes(Configuration["JwtSettings:SigningKey"]))
                     };
                 });
 
