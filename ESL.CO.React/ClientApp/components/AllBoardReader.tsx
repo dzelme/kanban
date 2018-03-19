7a0d1e3a05082cfa3584c18a36afc6a49091b4ac
arumka <arumka@RIX1503>
2018-03-19 15:47:45 +0200

    Pievienota komponente, kas prezentē visus paneļus

---
+++ ESL.CO.React/ClientApp/components/AllBoardReader.tsx
@@ -0,0 +1,40 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import ColumnReader from './ColumnReader';
+import { Value } from './Interfaces';
+
+interface BoardReaderState {
+    boardlist: Value[];
+    loading: boolean;
+}
+
+//Get all boards in list
+export class AllBoardReader extends React.Component<RouteComponentProps<{}>, BoardReaderState> {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            boardlist: [],
+            loading: true
+        };
+
+        fetch('api/SampleData/BoardList', {
+            headers: {
+                authorization: 'Bearer ' + sessionStorage.getItem('JwtToken')
+            }
+        })
+            .then(response => response.json() as Promise<Value[]>)
+            .then(data => {
+                this.setState({ boardlist: data, loading: false });
+            })
+    }
+
+    public render() {
+
+        let boardInfo = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : (this.state.boardlist.length != 0) ? <ColumnReader boardlist={this.state.boardlist} /> : <h1 >No boards</h1>
+
+        return <div>{boardInfo}</div>
+    }
+}
