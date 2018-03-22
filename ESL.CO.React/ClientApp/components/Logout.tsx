4e168774ae857f29e6f429313d256c06048ebf55
adzelme <arvids.dzelme@returnonintelligence.com>
2018-03-22 10:20:36 +0200

    atsevišķā atteikšanās komponente

---
+++ ESL.CO.React/ClientApp/components/Logout.tsx
@@ -0,0 +1,18 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+
+export class Logout extends React.Component<RouteComponentProps<{}> > {
+
+    constructor(props) {
+        super(props);
+        this.state = {}
+
+        sessionStorage.removeItem("JwtToken");
+        open('./login', '_self');
+    }
+
+    render() {
+        return null;
+    }
+
+}
