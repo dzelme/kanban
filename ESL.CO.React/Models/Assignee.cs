1648a4afaaff4623816aadee4d0326d7455e38be
arumka <arumka@RIX1503>
2018-03-22 10:57:12 +0200

    Sīkas izmaiņas koda izskata uzlabošanai

--- ESL.CO.React/Models/Assignee.cs
+++ ESL.CO.React/Models/Assignee.cs
@@ -7,14 +7,7 @@ namespace ESL.CO.React.Models
 {
     public class Assignee : IEquatable<Assignee>
     {
-        //public string self { get; set; }
-        //public string name { get; set; }  //username
-        //public string key { get; set; }
-        //public string emailAddress { get; set; }
-        //public AvatarUrls2 avatarUrls { get; set; }  //avatar image in 4 sizes (48x48, ..., 16x16)
-        public string DisplayName { get; set; }  //full name
-        //public bool active { get; set; }
-        //public string timeZone { get; set; }
+        public string DisplayName { get; set; } 
 
         public Assignee()
         {
