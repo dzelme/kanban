d163456564f84a67af587ddabc098e206f8060b3
adzelme <arvids.dzelme@returnonintelligence.com>
2018-04-04 12:34:43 +0300

    dzēšanas metodes labojums

--- ESL.CO.React/ClientApp/components/ApiClient.ts
+++ ESL.CO.React/ClientApp/components/ApiClient.ts
@@ -42,7 +42,7 @@ export class ApiClient {
     }
 
     // helper delete method
-    static delete(link): Promise<any> {
+    static delete(link) {
         return fetch(link, {
             method: 'DELETE',
             headers: {
