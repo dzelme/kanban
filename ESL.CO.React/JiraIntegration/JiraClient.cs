dcdda4200da94aa4d652edbd062e5f24e4ab1460
arumka <arumka@RIX1503>
2018-04-05 13:43:16 +0300

    Salabota darbība, pārmainīta jiraclient metode uz vienkāršāku

--- ESL.CO.React/JiraIntegration/JiraClient.cs
+++ ESL.CO.React/JiraIntegration/JiraClient.cs
@@ -34,7 +34,7 @@ namespace ESL.CO.React.JiraIntegration
             /// <returns>A type specific object corresponding to the JSON response from Jira REST API.</returns>
         public async Task<T> GetBoardDataAsync<T>(string url, string credentials, int id)
         {
-            var baseUri = new Uri("https://jira.returnonintelligence.com/rest/agile/1.0/");
+            var baseUri = new Uri("https://jira.returnonintelligence.com/rest/");
             var request = new HttpRequestMessage(HttpMethod.Get, new Uri(baseUri, url));
             request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes(credentials)));
             
@@ -59,37 +59,6 @@ namespace ESL.CO.React.JiraIntegration
             return default(T);  //null
             //throw new InvalidOperationException();
         }
-
-        public async Task<T> GetColorDataAsync<T>(string url, string credentials, int id)
-        {
-            var baseUri = new Uri("https://jira.returnonintelligence.com/rest/greenhopper/1.0/cardcolors/");
-
-            var request = new HttpRequestMessage(HttpMethod.Get, new Uri(baseUri, url));
-            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes(credentials)));
-
-            var response = await client.SendAsync(request);
-
-            if (response.IsSuccessStatusCode)
-            {
-                var serializer = new JsonSerializer();
-                using (var stream = await response.Content.ReadAsStreamAsync())
-                using (var reader = new StreamReader(stream))
-                using (var jsonReader = new JsonTextReader(reader))
-                {
-                    SaveToConnectionLog_AsTextFile(url, response, id);
-                    return serializer.Deserialize<T>(jsonReader);
-                }
-            }
-            else
-            {
-                //HttpError error = response.Content.ReadAsStringAsync().Result;
-            }
-
-            SaveToConnectionLog_AsTextFile(url, response, id);
-            return default(T);  //null
-            //throw new InvalidOperationException();
-        }
-
 
         /// <summary>
         /// Adds an entry to the appropriate connection log file.
