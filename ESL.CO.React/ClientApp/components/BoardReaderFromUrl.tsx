e32fb686ef0690e5a9805b874e33f26b3e295431
arumka <arumka@RIX1503>
2018-04-09 16:31:20 +0300

    Izmainīti nosaukumi arī atsevišķa paneļa komponentē

--- ESL.CO.React/ClientApp/components/BoardReaderFromUrl.tsx
+++ ESL.CO.React/ClientApp/components/BoardReaderFromUrl.tsx
@@ -48,8 +48,7 @@ export class BoardReaderFromUrl extends React.Component<RouteComponentProps<{ bo
     }
 
     boardLoad() {
-        clearInterval(this.refreshTimer);
-
+ 
         ApiClient.getPresentation(this.props.match.params.presentationId)
             .then(dataPres => {
 
@@ -70,14 +69,10 @@ export class BoardReaderFromUrl extends React.Component<RouteComponentProps<{ bo
     }
 
     RefreshRate() {
-        this.refreshTimer = setInterval(
-            () => this.boardLoad(),
-            this.state.boardList[0].refreshRate*1000
-        );
+        this.refreshTimer = setTimeout(this.boardLoad, this.state.boardList[0].refreshRate * 1000);
     }
 
-    //AD: increments timesShown board statistic
-    increment() {
+    updateStatistics() {
         ApiClient.saveToStatistics(this.state.board.id.toString(), this.state.board.name);
     }
 
@@ -105,7 +100,7 @@ export class BoardReaderFromUrl extends React.Component<RouteComponentProps<{ bo
                     <div>  <BoardName presentationId={this.props.match.params.presentationId} name={this.state.board.name} fromCache={this.state.board.fromCache} message={this.state.board.message} boardlist={this.state.boardList} /></div>
                     <div id='board'><BoardTable board={this.state.board} colorList={this.state.colorList} /></div>
 
-                    {this.increment()}
+                    {this.updateStatistics()}
 
                 </div>;
             }
