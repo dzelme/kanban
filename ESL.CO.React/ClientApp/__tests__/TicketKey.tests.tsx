546c7861ae711cea1b4e73e50d7222fd0875fa59
arumka <arumka@RIX1503>
2018-04-11 10:34:25 +0300

    Izveidota vide testu rakstīšanai. Uzrakstīts pirmais testa paraugs.

---
+++ ESL.CO.React/ClientApp/__tests__/TicketKey.tests.tsx
@@ -0,0 +1,13 @@
+﻿// Link.react.test.js
+import * as React from 'react';
+import TicketKey from '../components/TicketKey';
+import * as renderer from 'react-test-renderer';
+
+test('Renders keyname prop', () => {
+    const component = renderer.create(
+        <TicketKey keyName="123" />,
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
